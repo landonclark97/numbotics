8ceb5b96b9910754cc0027b781cac6561b380af1
landon <32660978+landonclark97@users.noreply.github.com>
2023-02-07 16:43:52 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='numerical-robotics',
+      version='0.0.1',
+      description='Numerical analysis library for robotics using Numpy',
+      author='Landon Clark',
+      author_email='landon.clark@uky.edu',
+      packages=['numbotics']
+)
