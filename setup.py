a3707a0570608ea05fbf025f0e97271a4ebf6ff1
BuildTools <unconfigured@null.spigotmc.org>
2023-02-07 17:45:17 -0500

    removing email '

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(name='numerical-robotics',
       version='0.0.1',
       description='Numerical analysis library for robotics using Numpy',
       author='Landon Clark',
-      author_email='landon.clark@uky.edu',
       packages=['numbotics']
 )
